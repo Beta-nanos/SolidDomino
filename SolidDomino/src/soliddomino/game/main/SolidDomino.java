a143ad4951824324bb46ad79d898c6044c04d74f
lisaula <luisca_elfather@hotmail.com>
2016-09-23 13:27:28 -0600

    Project initialization with NetBeans; base package with main created, but nothing done yet

---
+++ SolidDomino/src/soliddomino/game/main/SolidDomino.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soliddomino.game.main;
+
+/**
+ *
+ * @author lisaula
+ */
+public class SolidDomino {
+    public static void main(String[] args) {
+        
+    }
+}
